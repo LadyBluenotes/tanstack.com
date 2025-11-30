fd406f05d9e7433a8413b7f892665c80cd4f627d
Sarah Gerrard <98355961+LadyBluenotes@users.noreply.github.com>
2025-11-30 18:04:35 +0000

    chore(prettier): format eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,30 @@
-import { defineConfig } from "eslint/config";
-import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
-import reactHooks from "eslint-plugin-react-hooks";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from 'eslint/config'
+import { fixupConfigRules, fixupPluginRules } from '@eslint/compat'
+import reactHooks from 'eslint-plugin-react-hooks'
+import tsParser from '@typescript-eslint/parser'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
 
-export default defineConfig([{
-    extends: fixupConfigRules(compat.extends("react-app")),
+export default defineConfig([
+  {
+    extends: fixupConfigRules(compat.extends('react-app')),
 
     plugins: {
-        "react-hooks": fixupPluginRules(reactHooks),
+      'react-hooks': fixupPluginRules(reactHooks),
     },
 
     languageOptions: {
-        parser: tsParser,
+      parser: tsParser,
     },
-}]);
+  },
+])
